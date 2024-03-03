ae860e8c73139463fe5721e13a7b3aedc9ec186a
Christian Linde <info@christian-linde.de>
2024-03-03 12:05:56 +0100

    update 1.20.4

--- src/main/java/io/github/haykam821/packetlogger/PacketLogger.java
+++ src/main/java/io/github/haykam821/packetlogger/PacketLogger.java
@@ -2,23 +2,23 @@ package io.github.haykam821.packetlogger;
 
 import java.util.Locale;
 
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
+import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import io.github.haykam821.packetlogger.mixin.CustomPayloadC2SPacketAccessor;
-import io.github.haykam821.packetlogger.mixin.CustomPayloadS2CPacketAccessor;
 import net.minecraft.network.NetworkSide;
-import net.minecraft.network.Packet;
 import net.minecraft.util.Identifier;
 
 public class PacketLogger {
 	private static final Logger LOGGER = LogManager.getLogger("Packet Logger");
 
 	private static Identifier getChannel(Packet<?> packet) {
-		if (packet instanceof CustomPayloadC2SPacketAccessor) {
-			return ((CustomPayloadC2SPacketAccessor) packet).getChannel();
-		} else if (packet instanceof CustomPayloadS2CPacketAccessor) {
-			return ((CustomPayloadS2CPacketAccessor) packet).getChannel();
+		if (packet instanceof CustomPayloadC2SPacket) {
+			return ((CustomPayloadC2SPacket) packet).payload().id();
+		} else if (packet instanceof CustomPayloadS2CPacket) {
+			return ((CustomPayloadS2CPacket) packet).payload().id();
 		}
 		return null;
 	}
--- src/main/java/io/github/haykam821/packetlogger/mixin/ClientConnectionMixin.java
+++ src/main/java/io/github/haykam821/packetlogger/mixin/ClientConnectionMixin.java
@@ -1,5 +1,11 @@
 package io.github.haykam821.packetlogger.mixin;
 
+import io.github.haykam821.packetlogger.PacketLogger;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.NetworkSide;
+import net.minecraft.network.PacketCallbacks;
+import net.minecraft.network.listener.PacketListener;
+import net.minecraft.network.packet.Packet;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -7,26 +13,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import io.github.haykam821.packetlogger.PacketLogger;
-import io.netty.util.concurrent.GenericFutureListener;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.network.NetworkSide;
-import net.minecraft.network.Packet;
-import net.minecraft.network.listener.PacketListener;
-
 @Mixin(ClientConnection.class)
 public class ClientConnectionMixin {
 	@Shadow
 	@Final
 	private NetworkSide side;
 
-	@Inject(method = "sendImmediately", at = @At("HEAD"))
-	private void logSentPacket(Packet<?> packet, GenericFutureListener<?> callback, CallbackInfo ci) {
+	@Inject(method = "sendImmediately(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;Z)V", at = @At("HEAD"))
+	private void logSentPacket(Packet<?> packet, PacketCallbacks callbacks, boolean flush, CallbackInfo ci) {
 		PacketLogger.logSentPacket(packet, this.side);
 	}
 
-	@Inject(method = "handlePacket", at = @At("HEAD"))
+	@Inject(method = "handlePacket(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/listener/PacketListener;)V", at = @At("HEAD"))
 	private static void logReceivedPacket(Packet<?> packet, PacketListener listener, CallbackInfo ci) {
-		PacketLogger.logReceivedPacket(packet, ((ClientConnectionAccessor) listener.getConnection()).getSide());
+		PacketLogger.logReceivedPacket(packet,listener.getSide());
 	}
 }
